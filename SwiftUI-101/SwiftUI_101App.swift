876539613a1b74e404c47b75a93879b92b4815bd
Louis-Jacquin <louis.jacquin73@gmail.com>
2024-01-19 10:30:53 +0100

    Initial Commit

---
+++ SwiftUI-101/SwiftUI_101App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_101App.swift
+//  SwiftUI-101
+//
+//  Created by Louis JACQUIN on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
